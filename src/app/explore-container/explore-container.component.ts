34bb7a4ff94d0c199ba8a240a9ce97a5c592fb4c
Wagisigu <cjwagner4@wisc.edu>
2020-03-22 02:45:25 -0500

    animation started

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-explore-container',
@@ -6,17 +7,79 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
+  @ViewChild('imageCanvas', { static: false }) canvas: any;
+  canvasElement: any;
+  private ctx: CanvasRenderingContext2D;
+  originX: number;
+  originY: number;
+  saveX: number;
+  saveY: number;
+  icons: Array<{x: number, y: number, text: string}> = [];
+
+  drawing = false;
 
   clickCounter : number = 0;
 
-  constructor() { }
+  constructor(private plt: Platform) { }
 
   ngOnInit() {}
 
-  onClick() {
-    console.log('works');
+  ngAfterViewInit() {
+    // Set the Canvas Element and its size
+    this.canvasElement = this.canvas.nativeElement;
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+ 
+    this.originX = canvasPosition.x;
+    this.originY = canvasPosition.y;
+    this.canvasElement.width = this.plt.width()-2 + '';
+    this.canvasElement.height = this.plt.height()*.8 + '';
+    this.ctx = this.canvasElement.getContext('2d');
+  }
+
+  animate(): void {
+    this.ctx.clearRect(this.originX, this.originY, this.canvasElement.width,this.canvasElement.height);
+    this.icons.forEach((icon)=>{
+      this.ctx.fillStyle = icon.text;
+      this.ctx.beginPath();
+      this.ctx.arc(icon.x--,icon.y--,10,Math.PI*2,0,false);
+      this.ctx.closePath();
+      this.ctx.fill();
+    })
+    console.log('yo');
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  moved() {
+
+  }
+ 
+  startDrawing(ev) {
+    this.drawing = true;
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+ 
+    this.saveX = ev.pageX - canvasPosition.x;
+    this.saveY = ev.pageY - canvasPosition.y;
+    this.icons.push({x:this.saveX,y:this.saveY,text:'blue'});
+
+    this.ctx.fillStyle = '#9e2956';
+    this.ctx.beginPath();
+    this.ctx.arc(this.saveX,this.saveY,10,Math.PI*2,0,false);
+    this.ctx.closePath();
+    this.ctx.fill();
+    this.animate();
+  }
+ 
+  endDrawing() {
+    this.drawing = false;
+  }
+
+  onStartClick() {
+    console.log('add');
     this.clickCounter++;
+  }
+
+  onChartClick() {
+    console.log('chart');
   }
 
 }

cfbc4b0d70c9e786479172b59a74d4ba54303bc3
Wagisigu <cjwagner4@wisc.edu>
2020-03-23 19:32:10 -0500

    buttons can be generated in expanding canvas

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Platform, IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-explore-container',
@@ -12,11 +12,11 @@ export class ExploreContainerComponent implements OnInit {
   private ctx: CanvasRenderingContext2D;
   originX: number;
   originY: number;
+  centerX: number;
+  centerY: number;
   saveX: number;
   saveY: number;
   icons: Array<{x: number, y: number, text: string}> = [];
-
-  drawing = false;
 
   clickCounter : number = 0;
 
@@ -33,44 +33,56 @@ export class ExploreContainerComponent implements OnInit {
     this.originY = canvasPosition.y;
     this.canvasElement.width = this.plt.width()-2 + '';
     this.canvasElement.height = this.plt.height()*.8 + '';
+    this.centerX = this.originX + this.canvasElement.width/2;
+    this.centerY = this.originY + this.canvasElement.height/2;
     this.ctx = this.canvasElement.getContext('2d');
   }
 
   animate(): void {
     this.ctx.clearRect(this.originX, this.originY, this.canvasElement.width,this.canvasElement.height);
-    this.icons.forEach((icon)=>{
-      this.ctx.fillStyle = icon.text;
+    for (let i = 0; i < this.icons.length; i++) {
+      if (i == 0) {
+        this.icons[i].x = this.centerX;
+        this.icons[i].y = this.centerY;
+      }
+      else {
+        this.icons[i].x = this.centerX + (Math.floor((i-1)/6)+1)*60*Math.sin(i);
+        this.icons[i].y = this.centerY + (Math.floor((i-1)/6)+1)*60*Math.cos(i);
+      }
+      this.ctx.fillStyle = this.icons[i].text;
       this.ctx.beginPath();
-      this.ctx.arc(icon.x--,icon.y--,10,Math.PI*2,0,false);
+      this.ctx.arc(this.icons[i].x,this.icons[i].y,25,Math.PI*2,0,false);
       this.ctx.closePath();
       this.ctx.fill();
-    })
-    console.log('yo');
+      if (this.icons[i].x - this.canvasElement.width > 0) {
+        let expansion = this.icons[i].x - this.canvasElement.width;
+        this.canvasElement.width += expansion*4;
+        this.canvasElement.height += expansion*4;
+        this.centerX = this.originX + this.canvasElement.width/2;
+        this.centerY = this.originY + this.canvasElement.height/2;
+        i=0;
+      }
+    }
     requestAnimationFrame(this.animate.bind(this));
-  }
-
-  moved() {
-
   }
  
   startDrawing(ev) {
-    this.drawing = true;
     var canvasPosition = this.canvasElement.getBoundingClientRect();
  
     this.saveX = ev.pageX - canvasPosition.x;
     this.saveY = ev.pageY - canvasPosition.y;
-    this.icons.push({x:this.saveX,y:this.saveY,text:'blue'});
-
-    this.ctx.fillStyle = '#9e2956';
-    this.ctx.beginPath();
-    this.ctx.arc(this.saveX,this.saveY,10,Math.PI*2,0,false);
-    this.ctx.closePath();
-    this.ctx.fill();
+    let buttonHit = false;
+    for (let i = 0; i < this.icons.length; i++) {
+      if (Math.sqrt(Math.pow(this.icons[i].x-this.saveX,2)+Math.pow(this.icons[i].y-this.saveY,2)) < 25) {
+        console.log(i);
+        buttonHit = true;
+        break;
+      }
+    }
+    if (!buttonHit) {
+      this.icons.push({x:this.saveX,y:this.saveY,text:'rgb(' + Math.random()*99 + ',' + Math.random()*99+','+Math.random()*99+')'});
+    }
     this.animate();
-  }
- 
-  endDrawing() {
-    this.drawing = false;
   }
 
   onStartClick() {

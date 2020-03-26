5e42e346099068ee498a1b2719ecf077770d538f
Wagisigu <cjwagner4@wisc.edu>
2020-03-26 17:07:10 -0500

    circle are named

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -17,7 +17,7 @@ export class ExploreContainerComponent implements OnInit {
   centerY: number;
   saveX: number;
   saveY: number;
-  icons: Array<{x: number, y: number, text: string}> = [];
+  icons: Array<{x: number, y: number, title: string, why:string, color:string}> = [];
 
   clickCounter : number = 0;
 
@@ -51,7 +51,7 @@ export class ExploreContainerComponent implements OnInit {
     modal.onWillDismiss().then(dataReturned => {
       var name = dataReturned.data;
       console.log("received" + name);
-      this.icons.push({x:0,y:0,text:'rgb(' + Number.parseInt(name) + ',' + 0 +','+ 0 +')'});
+      this.icons.push({x:this.saveX,y:this.saveY,title:name,why:'',color:'rgb(' + Math.random()*99 + ',' + Math.random()*99+','+Math.random()*99+')'});
       this.animate();
     });
 
@@ -68,14 +68,17 @@ export class ExploreContainerComponent implements OnInit {
         this.icons[i].y = this.centerY;
       }
       else {
-        this.icons[i].x = this.centerX + (Math.floor((i-1)/6)+1)*60*Math.sin(i);
-        this.icons[i].y = this.centerY + (Math.floor((i-1)/6)+1)*60*Math.cos(i);
+        this.icons[i].x = this.centerX + (Math.floor((i-1)/6)+1)*110*Math.sin(i);
+        this.icons[i].y = this.centerY + (Math.floor((i-1)/6)+1)*110*Math.cos(i);
       }
-      this.ctx.fillStyle = this.icons[i].text;
+      this.ctx.fillStyle = this.icons[i].color;
       this.ctx.beginPath();
-      this.ctx.arc(this.icons[i].x,this.icons[i].y,25,Math.PI*2,0,false);
+      this.ctx.arc(this.icons[i].x,this.icons[i].y,50,Math.PI*2,0,false);
       this.ctx.closePath();
       this.ctx.fill();
+      this.ctx.fillStyle = "white";
+      this.ctx.textAlign = "center";
+      this.ctx.fillText(this.icons[i].title,this.icons[i].x,this.icons[i].y,96);
       if (this.icons[i].x - this.canvasElement.width > 0) {
         let expansion = this.icons[i].x - this.canvasElement.width;
         this.canvasElement.width += expansion*4;
@@ -88,21 +91,18 @@ export class ExploreContainerComponent implements OnInit {
     requestAnimationFrame(this.animate.bind(this));
   }
  
-  startDrawing(ev) {
+  startDrawing(event) {
     var canvasPosition = this.canvasElement.getBoundingClientRect();
  
-    this.saveX = ev.pageX - canvasPosition.x;
-    this.saveY = ev.pageY - canvasPosition.y;
+    this.saveX = event.pageX - canvasPosition.x;
+    this.saveY = event.pageY - canvasPosition.y;
     let buttonHit = false;
     for (let i = 0; i < this.icons.length; i++) {
-      if (Math.sqrt(Math.pow(this.icons[i].x-this.saveX,2)+Math.pow(this.icons[i].y-this.saveY,2)) < 25) {
-        console.log(i);
+      if (Math.sqrt(Math.pow(this.icons[i].x-this.saveX,2)+Math.pow(this.icons[i].y-this.saveY,2)) < 50) {
+        console.log(this.icons[i].title);
         buttonHit = true;
         break;
       }
-    }
-    if (!buttonHit) {
-      this.icons.push({x:this.saveX,y:this.saveY,text:'rgb(' + Math.random()*99 + ',' + Math.random()*99+','+Math.random()*99+')'});
     }
     this.animate();
   }

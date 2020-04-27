e6f2acd06eae2727fcbae50f375df2ee69925739
Cameron Wagner <cjwagner4@wisc.edu>
2020-04-27 03:45:40 -0500

    canvas ready

--- src/app/datagraphics/datagraphics.page.ts
+++ src/app/datagraphics/datagraphics.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-datagraphics',
@@ -7,10 +8,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./datagraphics.page.scss'],
 })
 export class DatagraphicsPage implements OnInit {
+  @ViewChild('imageCanvas', { static: false }) canvas: any;
+  canvasElement: any;
+  private ctx: CanvasRenderingContext2D;
+  originX: number;
+  originY: number;
+  centerX: number;
+  centerY: number;
+  data: Array<{date: {d:number,m:number,y:number}, metric: number}>;
 
-  constructor(private router: Router) { }
+  constructor(private plt: Platform, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    // Set the Canvas Element and its size
+    this.canvasElement = this.canvas.nativeElement;
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+ 
+    this.originX = canvasPosition.x;
+    this.originY = canvasPosition.y;
+    this.canvasElement.width = this.plt.width()-2 + '';
+    this.canvasElement.height = this.plt.height()*.8 + '';
+    this.centerX = this.originX + this.canvasElement.width/2;
+    this.centerY = this.originY + this.canvasElement.height/2;
   }
 
   return() {

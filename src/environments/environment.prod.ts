28b765840779aadffefde627fb3e461926239f67
Nick <petersonn74@gmail.com>
2020-05-02 11:55:13 -0500

    Add fire packages and setup firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDh916sGQcNbt0HAvGezuotjGz7_2JTvI0",
+    authDomain: "project-8985a.firebaseapp.com",
+    databaseURL: "https://project-8985a.firebaseio.com",
+    projectId: "project-8985a",
+    storageBucket: "project-8985a.appspot.com",
+    messagingSenderId: "67490715433",
+    appId: "1:67490715433:web:871f049546bc0236010938"
+  }
 };

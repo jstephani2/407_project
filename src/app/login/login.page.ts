7dd6372af721dbee0a3ac183b252c5cb030bf886
Nick <petersonn74@gmail.com>
2020-05-02 16:12:10 -0500

    Add register/login functions

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+
+export class User {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public user:User = new User();
+
+  constructor( public fAuth: AngularFireAuth ) { }
 
   ngOnInit() {
+  }
+
+  register() {
+    this.fAuth.createUserWithEmailAndPassword(this.user.email,this.user.password)
+    .then((res) => { if (res.user) console.log(res.user.uid) })
+    .catch((e) => { console.log(e); })
+
+  }
+  login() {
+    this.fAuth.signInWithEmailAndPassword(this.user.email,this.user.password)
+    .then((res) => { console.log(res.user.uid); })
+    .catch((e) => { console.log(e); })
   }
 
 }
